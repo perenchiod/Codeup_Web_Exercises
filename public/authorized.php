2ae2482fea6314514884353c459306da6745ed18
Dylan Perenchio <perenchiod@gmail.com>
2015-07-22 15:19:24 -0500

    Working log-in username and password field in PHP

--- public/authorized.php
+++ public/authorized.php
@@ -1,8 +1,20 @@
-	<html>
+<?php 
+	session_start();
+	$session_Id = session_id();
+	if($_SESSION["username"] != 'nightness' && $_SESSION["password"] != 'alrightalright'){
+		header('Location: http://codeup.dev/login.php');
+		exit(); 
+	}
+
+?>
+
+<html>
 <head>
 	<title>YOU GOT AUTHORIZED!</title>
 </head>
 <body>
-	<h1>Authorized</h1>
+	<h1>Authorized <? echo $_SESSION["username"]; ?></h1>
+	Session Id: <?php echo $session_Id; ?>
+	<a href="http://codeup.dev/logout.php">Logout</a>
 </body>
 </html>
---
+++ public/logout.php
@@ -0,0 +1,34 @@
+<?php 
+	session_start();
+	function endSession()
+	{
+    // Unset all of the session variables.
+    $_SESSION = array();
+
+    // If it's desired to kill the session, also delete the session cookie.
+    // Note: This will destroy the session, and not just the session data!
+    if (ini_get("session.use_cookies")) {
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time() - 42000,
+            $params["path"], $params["domain"],
+            $params["secure"], $params["httponly"]
+        );
+    }
+
+    // Finally, destroy the session.
+    session_destroy();
+    header('Location: http://codeup.dev/login.php');
+	exit(); 
+}
+
+?>
+
+
+<html>
+<head>
+	<title>Logout</title>
+</head>
+<body>
+	<? endSession() ?>
+</body>
+</html>
