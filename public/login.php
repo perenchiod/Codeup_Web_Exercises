f0ab0f3855ddb32f8fdd69c8d0fc3a8ce654ed74
Dylan Perenchio <perenchiod@gmail.com>
2015-07-22 15:22:19 -0500

    uploading

--- public/login.php
+++ public/login.php
@@ -1,13 +1,19 @@
 <?php
-	
+	session_start();
 	 if(isset($_POST['username']) && isset($_POST['password'])) { 
+		
+		$_POST['username'] = strtolower($_POST['username']);
+		$_SESSION["username"] = ($_POST['username']);
+		$_SESSION["password"] = $_POST['password'];
+		
 		if($_POST['username'] == 'nightness' && $_POST['password'] == 'alrightalright'){ 
-			header('Location: http://codeup.dev/authorized.php');      
+			header('Location: http://codeup.dev/authorized.php');
+			exit();      
 		} else {
 			$message = "Incorrect username or password";
 			echo "<script type='text/javascript'> alert('$message'); </script>";
 		}
-	}
+	} 
 	
 ?>
 <script src="js/bootstrap.min.js"></script>
@@ -21,7 +27,9 @@
         <input type="text" name="username"><br>
         <label>Password</label>
         <input type="password" name="password"><br>
-        <input type="submit" href="authorized.php">		
+        <input type="submit" href="authorized.php">	
+        <p>Username: <? if(isset($_POST['username'])) { echo htmlspecialchars(strip_tags($_POST['username'])); } ?>  </p>	
+        <p>Password: <? if(isset($_POST['password'])) { echo htmlspecialchars(strip_tags($_POST['password'])); } ?>  </p>	
     </form>
 </body>
 </html>
