a7f91b1e21d64ef08ebb4668e8233c51c2d0cf06
Dylan Perenchio <perenchiod@gmail.com>
2015-08-02 22:01:59 -0500

    updated to victory

--- db_connect.php
+++ db_connect.php
@@ -1,15 +1,9 @@
 <?php
 
-$DB_HOST = '127.0.0.1';
-$DB_NAME = 'employees';
-$DB_USER = 'codeup';
-$DB_PASS = 'tibbers';
+	// Get new instance of PDO object
+	$dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 
-
-// Get new instance of PDO object
-$dbc = new PDO('mysql:host=$DB_HOST;dbname=$DB_NAME', $DB_USER, $DB_PASS);
-
-// Tell PDO to throw exceptions on error
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	// Tell PDO to throw exceptions on error
+	$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 ?>
--- pdo_test.php
+++ pdo_test.php
@@ -1,16 +1,11 @@
 <?php
-	// Get new instance of PDO object
-$dbc = new PDO('mysql:host=127.0.0.1;dbname=employees', 'codeup' , 'tibbers');
-
-// Tell PDO to throw exceptions on error
-$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
-
-
-
-
-
+	require 'db_connect.php';
+	define("DB_HOST", '127.0.0.1');
+	define("DB_NAME", 'employees');
+	define("DB_USER", 'codeup');
+	define("DB_PASS", 'PASSWORD');
+	
+	echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
 
 
 
