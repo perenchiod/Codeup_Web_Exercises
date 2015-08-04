7bb5c80d6b8ffa5bd910a2fd295d34fb622ae6d5
Dylan Perenchio <perenchiod@gmail.com>
2015-08-04 13:46:34 -0500

    Added some new features to national_parks

--- public/national_parks.php
+++ public/national_parks.php
@@ -4,21 +4,55 @@
 	define("DB_USER",'parks_user');
 	define("DB_PASS",'');
 	require '../db_connect.php';
+	require 'functions.php';
+
 	
 	$limitVar = 4;
 	$parks = $dbc->query('SELECT * FROM national_parks LIMIT ' . $limitVar);
 	$count = $dbc->query('SELECT count(*) FROM national_parks');
 
+	//Is page GET page empty? If so page number = 0
 	if(empty($_GET['page'])) {
 		$_GET['page'] = 0;
 	} 
 	
+	//Seeing if GET page has a value and does calculation based of page
 	if(isset($_GET['page'])) 
 	{
 		$offsetVar = $limitVar * $_GET['page'];
 		$parks = $dbc->query('SELECT * FROM national_parks LIMIT ' . $limitVar . ' OFFSET ' . $offsetVar);
 	}
 
+	//Perpared stmt variable used for inserting data into database
+	$stmt = $dbc->prepare("INSERT INTO national_parks (name,location,date_established,area_in_acres, description) VALUES (
+		:name, 
+		:location, 
+		:date_established, 
+		:area_in_acres, 
+		:description
+	)");
+	
+	if(inputHas("name") && inputHas("location") && inputHas("date_established") && inputHas("area_in_acres")) {
+		$nameVar = escape(inputGet("name"));
+		$locationVar = escape(inputGet('location'));
+		$establishedVar = escape(inputGet('date_established'));
+		$acresVar = escape(inputGet('area_in_acres'));
+		
+		$stmt->bindValue(':name' , $nameVar , PDO::PARAM_STR);
+		$stmt->bindValue(':location' , $locationVar , PDO::PARAM_STR);
+		$stmt->bindValue(':date_established' , $establishedVar , PDO::PARAM_STR);
+		$stmt->bindValue(':area_in_acres' , $acresVar , PDO::PARAM_INT);
+		
+		//Conditional checking if a description was entered
+		if(inputHas($_POST['description'])) {
+			$descriptionVar = escape(inputGet('description'));
+			$stmt->bindValue(':description' , $_POST['description'] , PDO::PARAM_STR);
+		} else {
+			$stmt->bindValue(':description' , 'none' , PDO::PARAM_STR);
+		}
+		//Execute the stmt, this will give me a new entry in database
+		$stmt->execute();
+	}
 ?>
 
 <html>
@@ -29,11 +63,11 @@
 	<link rel="stylesheet" type="text/css" href="/css/national_parks.css">
 </head>
 <body>
-	<h1>Page: <?php echo $_GET['page'];  ?>
+	<h1>National Parks! </h1>
 	<table class="table table-striped">
 		<th>Name</th>
 		<th>Location</th>
-		<th>Date Established</th>
+		<th>Date Established (year/month/day)</th>
 		<th>Area (acres)</th>
 		<?php foreach ($parks as $park) { ?>
 			<tr>
@@ -51,6 +85,20 @@
 	<?php if($_GET['page'] >= 1) { ?>
 		<a id='back' href='http://codeup.dev/national_parks.php?page=<?php if(isset($_GET['page'])){ echo $_GET['page']-1; } ?>'>Back</a>
 	<?php } ?>
+	<form method="POST">
+		<h3>Park name</h3>
+		<input type="text" class="input" name="name" placeholder="Park name"> <br>
+		<h3>Location of park</h3>
+		<input type="text" class="input" name="location" placeholder="Location of park"> <br>
+		<h3>Date park was founded, Year/Mounth/Day</h3>
+		<input type="text" class="input" name="date_established" placeholder="Date park establishment"> <br>
+		<h3>Acres of park</h3>
+		<input type="text" class="input" name="area_in_acres" placeholder="Area of park in acres"> <br>
+		<input type="textarea" class="input" name="description" placeholder="Description"> <br>
+		<input type="submit">
+	</form>
 
+	<p><? if(inputHas("name")) { echo $_POST['name']; } ?>  </p>	
+	<p><? if(inputHas("location")) { echo $_POST['location']; } ?>  </p>	
 </body>
 </html>
